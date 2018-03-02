6ae45c4dd1f0f43bf9457f8626ee24e39b087dd4
tomwey <tomwey@163.com>
2018-03-02 15:27:04 +0800

    add some func

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,26 +13,27 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap
-//= require app
+//= require mate-tpl/popper.min
+//= require mate-tpl/bootstrap.min
+//= require mate-tpl/jquery.mixitup
+//= require mate-tpl/nivo-lightbox
+//= require mate-tpl/owl.carousel
+//= require mate-tpl/jquery.stellar.min
+//= require mate-tpl/jquery.nav
+//= require mate-tpl/scrolling-nav
+//= require mate-tpl/jquery.easing.min
+//= require mate-tpl/smoothscroll
+//= require mate-tpl/jquery.slicknav
+//= require mate-tpl/wow
+//= require mate-tpl/jquery.vide
+//= require mate-tpl/jquery.counterup.min
+  
+//= require mate-tpl/jquery.magnific-popup.min
+//= require mate-tpl/waypoints.min
+//= require mate-tpl/main
 
 //= require redactor-rails/redactor
 //= require redactor-rails/config
 //= require redactor-rails/langs/zh_cn
 //= require redactor-rails/plugins
 
-$(document).ready(function() {
-  $("#fixed-hb").hide();
-  $("#random-hb").show();
-  $("#hb-type").change(function() {
-    // console.log($(this).val());
-    var val = $(this).val();
-    if (val === '0') {
-      $("#fixed-hb").hide();
-      $("#random-hb").show();
-    } else {
-      $("#random-hb").hide();
-      $("#fixed-hb").show();
-    }
-  })
-});
